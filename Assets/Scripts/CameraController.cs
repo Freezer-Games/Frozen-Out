a92265b36b9c66a5e607c44e7fed5d1d3151de59
anleus <tomas.rui598@gmail.com>
2019-10-29 18:41:04 +0100

    mov y camara basica, falta arreglar cosas de camara y collider

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValue;
+    public float rotateSpeed;
+    public Transform pivot; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (useOffsetValue) 
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //get X pos raton y rota el target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0,horizontal, 0);
+
+        //get X pos raton y rota el pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        //Mueve camara según rotación actual del target y offset original
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        if (transform.position.y < target.position.y) 
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, target.position.z);
+        }
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeController : MonoBehaviour
+{
+    //public Rigidbody rb;
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+       controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed,rb.velocity.y,  Input.GetAxis("Vertical")*moveSpeed);
+
+        /*if(Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }*/
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y,  Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + 
+                        (transform.right * Input.GetAxis("Horizontal"));
+
+        moveDirection = moveDirection.normalized * moveSpeed; 
+        moveDirection.y = yStore;
+
+        if (controller.isGrounded) 
+        {
+            moveDirection.y = 0; 
+            if(Input.GetButtonDown("Jump")) 
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
