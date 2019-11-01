d4d59382e9377f78e3b60c0ff56c2594715b26ec
n2t <vicentplambng@gmail.com>
2019-11-01 22:08:43 +0100

    Dialogo extendido

---
+++ Assets/Scripts/TestVariablesYarn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class TestVariablesYarn : MonoBehaviour
+{
+    VariableStorageBehaviour variableStorageYarn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        variableStorageYarn = FindObjectOfType<VariableStorageBehaviour>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player") {
+            variableStorageYarn.SetValue("$reached_top", new Yarn.Value(true));
+        }
+    }
+}
