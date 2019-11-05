1a4bcff0fe05a077a250de1e56f542ab052d10b9
anleus <tomas.rui598@gmail.com>
2019-11-05 21:26:18 +0100

    version 2 de camara y movimiento (salto no esta aun pero es easy), prefab de Pol con estas cosas añadido tambien

---
+++ Assets/Scripts/V2CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V2CameraController : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate() 
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -17, 60);
+
+        transform.LookAt(Target);
+
+        if (Input.GetKey(KeyCode.Mouse2)) 
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else 
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/V2PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V2PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
