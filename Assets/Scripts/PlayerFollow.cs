adfcddf2a1f4a72d7343d1f5e4de04771d8360be
anleus <tomas.rui598@gmail.com>
2019-11-06 16:10:03 +0100

    movimiento de la hostia con camara como yo queria de una puta vez :))

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationsSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        _cameraOffset = transform.position - PlayerTransform.position;	
+	}
+	
+	// LateUpdate is called after Update methods
+	void LateUpdate () {
+
+        if(RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle =
+                Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
+
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+            transform.LookAt(PlayerTransform);
+	}
+}
