40707660ea616134b674a568676694f9378f88ca
Plorrios <pablolopezorrios@hotmail.es>
2019-11-11 14:35:19 +0100

    Menu
    Comenzado un diseño sencillo de interfaz

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public Button ConfirmButton;
+    public Button CancelButton;
+    public Canvas MainCanvas;
+    public Canvas OptionsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OptionsCanvas.enabled = false;
+    }
+
+    void confirm()
+    {
+
+        MainCanvas.enabled = true;
+        OptionsCanvas.enabled = false;
+
+    }
+
+    void cancel()
+    {
+
+        MainCanvas.enabled = true;
+        OptionsCanvas.enabled = false;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConfirmButton.onClick.AddListener(confirm);
+        CancelButton.onClick.AddListener(cancel);
+    }
+}
