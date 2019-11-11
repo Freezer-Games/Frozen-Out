cd8860dfe2d06836022504f05108592d73d2cd48
Plorrios <pablolopezorrios@hotmail.es>
2019-11-11 18:42:13 +0100

    Cambios a la interfaz
    Ajustada la interfaz al tamaño de pantalla y añadidas nuevas opciones

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -11,11 +11,28 @@ public class OptionsMenu : MonoBehaviour
     public Button CancelButton;
     public Canvas MainCanvas;
     public Canvas OptionsCanvas;
+    public Button GameButton;
+    public Canvas GameCanvas;
+    public Button AudioButton;
+    public Canvas AudioCanvas;
+    public Button GraphicsButton;
+    public Canvas GraphicsCanvas;
+    public Button ControlsButton;
+    public Canvas ControlsCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         OptionsCanvas.enabled = false;
+        AudioCanvas.enabled = false;
+        GraphicsCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+        ConfirmButton.onClick.AddListener(confirm);
+        CancelButton.onClick.AddListener(cancel);
+        GameButton.onClick.AddListener(Game);
+        AudioButton.onClick.AddListener(Audio);
+        GraphicsButton.onClick.AddListener(Graphics);
+        ControlsButton.onClick.AddListener(Controls);
     }
 
     void confirm()
@@ -34,11 +51,49 @@ public class OptionsMenu : MonoBehaviour
 
     }
 
+    void Game()
+    {
+
+        GameCanvas.enabled = true;
+        AudioCanvas.enabled = false;
+        GraphicsCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+
+    }
+
+    void Audio()
+    {
+
+        GameCanvas.enabled = false;
+        AudioCanvas.enabled = true;
+        GraphicsCanvas.enabled = false;
+        ControlsCanvas.enabled = false;
+
+    }
+
+    void Graphics()
+    {
+
+        GameCanvas.enabled = false;
+        AudioCanvas.enabled = false;
+        GraphicsCanvas.enabled = true;
+        ControlsCanvas.enabled = false;
+
+    }
+
+    void Controls()
+    {
+
+        GameCanvas.enabled = false;
+        AudioCanvas.enabled = false;
+        GraphicsCanvas.enabled = false;
+        ControlsCanvas.enabled = true;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        ConfirmButton.onClick.AddListener(confirm);
-        CancelButton.onClick.AddListener(cancel);
+
     }
 }
