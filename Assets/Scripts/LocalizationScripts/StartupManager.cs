1bb16e0bb586825023d751fb102b514615cdfb3c
Plorrios <pablolopezorrios@hotmail.es>
2019-11-14 14:07:32 +0100

    Localización en menu principal
    Terminados scripts de localización y permite cambiar el idioma del menú principal entre inglés y español

---
+++ Assets/Scripts/LocalizationScripts/StartupManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class StartupManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+
+        while (!LocalizationManager.instance.GetIsReady())
+        {
+            yield return null; }
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+
+}
---
+++ Assets/Scripts/Menu/GameCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public Dropdown DropdownLanguage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DropdownLanguage.onValueChanged.AddListener(ChangeLanguageF);
+    }
+
+    void ChangeLanguageF(int selection)
+    {
+
+        if (selection == 0) { LocalizationManager.instance.LoadLocalizedText("Menu_En.json"); }
+        else if (selection == 1) { LocalizationManager.instance.LoadLocalizedText("Menu_Es.json"); }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
