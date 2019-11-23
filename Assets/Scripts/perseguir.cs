4c58bb1e8b2a8d52132e3e73f975e1856b79c010
Plorrios <pablolopezorrios@hotmail.es>
2019-11-23 23:46:57 +0100

    Arreglados fallos de perseguir y menu

--- Assets/Scripts/perseguir.cs
+++ Assets/Scripts/perseguir.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class perseguir : MonoBehaviour
 {
+
     // Update is called once per frame
     void Update()
     {
-        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
         List<Transform> visibles = gameObject.GetComponent<FieldOfView>().visibleTargets;
         List<Transform> cercanos = gameObject.GetComponent<FieldOfView>().closeTargets;
+        Debug.Log("vivibles: "+visibles.Count);
+        Debug.Log("cercanos: "+cercanos.Count);
         if (visibles.Count==1 && cercanos.Count==0) { agent.destination = visibles[0].position; }
     }
 }
