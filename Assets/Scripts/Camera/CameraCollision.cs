371ece98dae84005678ec83e25504d68ad3b3a91
Tomás <tomas.rui598@gmail.com>
2019-11-24 23:24:16 +0100

    refactoring scripts de camara

--- Assets/Scripts/Camera/CameraCollision.cs
+++ Assets/Scripts/Camera/CameraCollision.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CameraCollision : MonoBehaviour
 {
+    [Header("Ray start")]
     public Transform character;
     public Transform leftRay;
     public Transform rightRay;
+
     public Material transparencia;
 
     private Dictionary<string,Transform> viewcolisions = new Dictionary<string, Transform>();
@@ -18,25 +20,28 @@ public class CameraCollision : MonoBehaviour
     private Vector3 camPosition;
     private Transform initPos;
     private Vector3 characterPosition;
+    private RaycastHit[] hits;
     private float newCamDistance;
     private Vector3 addedCharacterPos = new Vector3(0, 1.3f, 0);
+    private int layerMask = 1 << 9;
 
     void Start() 
     {
         cam = Camera.main;
-        initPos = cam.GetComponent<BetterCamera>().endOfRay.transform;
+        initPos = cam.GetComponent<CameraController>().endOfRay.transform;
     }
     void Update()
     {
-        initPos = cam.GetComponent<BetterCamera>().endOfRay.transform;
+        initPos = cam.GetComponent<CameraController>().endOfRay.transform;
         characterPosition = character.position + addedCharacterPos;
 
         direction = initPos.position - characterPosition;
-
-        int layerMask = 1 << 9;
         objetos.Clear();
 
-        RaycastHit[] hits;
+        RayTracing(); 
+    }
+
+    private  void RayTracing() {
         hits = Physics.RaycastAll(characterPosition, direction, direction.magnitude, layerMask);
 
         try {
@@ -45,28 +50,13 @@ public class CameraCollision : MonoBehaviour
                 RaycastHit hit = hits[i];
                 string objeto = hit.transform.gameObject.name;
                 Transform pos = hit.transform;
-                if (hit.transform.gameObject.tag == "Wall") 
+                if (hit.transform.gameObject.tag == "Wall")
                 {
-                    newCamDistance = (hits[0].transform.position  - characterPosition).magnitude;
-
-                    cam.GetComponent<BetterCamera>().distance = newCamDistance + 1.0f;
-
-                    viewcolisions.Add(objeto, pos);
-                    materiales.Add(objeto, pos.GetComponent<Renderer>().material);
-                    pos.GetComponent<MeshRenderer>().enabled = false;
+                    ZoomInCamera(pos, hit, objeto);
                 }
                 else 
                 {
-                    if (pos)
-                    {
-                        if (!viewcolisions.ContainsKey(objeto))
-                        {
-                            viewcolisions.Add(objeto, pos);
-                            materiales.Add(objeto, pos.GetComponent<Renderer>().material);
-                            pos.GetComponent<Renderer>().material = transparencia;
-                            //Debug.Log("añade");
-                        }
-                    }
+                    MakeItTransparent(pos, hit, objeto);
                 }  
                 //Debug.Log("hit");
                 objetos.Add(objeto);
@@ -79,37 +69,68 @@ public class CameraCollision : MonoBehaviour
                 if (hits.Length == 0)
                 {
                     //Debug.Log("elimina0");
-                    if (viewcolisions[key].gameObject.tag == "Wall") {
-                        cam.GetComponent<BetterCamera>().distance = direction.magnitude;
-                        viewcolisions[key].GetComponent<MeshRenderer>().enabled = true;
-                        viewcolisions.Remove(key);
-                        materiales.Remove(key);
+                    if (viewcolisions[key].gameObject.tag == "Wall") 
+                    {
+                        ReleaseZoom(key);
                     }
                     else 
                     {
-                        viewcolisions[key].GetComponent<Renderer>().material = materiales[key];
-                        viewcolisions.Remove(key);
-                        materiales.Remove(key);
+                        ReleaseMaterial(key);
                     }        
                 } 
                 else if (!objetos.Contains(key))
                 {
-                    if (viewcolisions[key].gameObject.tag == "Wall") {
-                        cam.GetComponent<BetterCamera>().resetDistance();
-                        viewcolisions[key].GetComponent<MeshRenderer>().enabled = true;
-                        viewcolisions.Remove(key);
-                        materiales.Remove(key);
+                    if (viewcolisions[key].gameObject.tag == "Wall") 
+                    {
+                        ReleaseZoom(key);
                     }
                     else 
                     {
-                        viewcolisions[key].GetComponent<Renderer>().material = materiales[key];
-                        viewcolisions.Remove(key);
-                        materiales.Remove(key);
+                        ReleaseMaterial(key);
                     }
                     //Debug.Log("elimina1");  
                 }
             }
         } catch {}
+    }
+
+    private void ZoomInCamera(Transform pos, RaycastHit hit, string objeto) 
+    {
+        newCamDistance = (hits[0].transform.position  - characterPosition).magnitude;
+
+        cam.GetComponent<CameraController>().distance = newCamDistance + 1.0f;
+
+        viewcolisions.Add(objeto, pos);
+        materiales.Add(objeto, pos.GetComponent<Renderer>().material);
+        pos.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void MakeItTransparent(Transform pos, RaycastHit hit, string objeto) 
+    {
+        if (pos)
+        {
+            if (!viewcolisions.ContainsKey(objeto))
+            {
+                viewcolisions.Add(objeto, pos);
+                materiales.Add(objeto, pos.GetComponent<Renderer>().material);
+                pos.GetComponent<Renderer>().material = transparencia;
+            }
+        }
+    }
+
+    private void ReleaseZoom(string key) 
+    {
+        cam.GetComponent<CameraController>().distance = direction.magnitude;
+        viewcolisions[key].GetComponent<MeshRenderer>().enabled = true;
+        viewcolisions.Remove(key);
+        materiales.Remove(key);
+    }
+
+    private void ReleaseMaterial(string key) 
+    {
+        viewcolisions[key].GetComponent<Renderer>().material = materiales[key];
+        viewcolisions.Remove(key);
+        materiales.Remove(key);
     }
 }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public BetterCamera betterCamera; //hacer un cameraManager
+    public CameraController cameraController; //hacer un cameraManager
     public GameObject playerObject;
     public PlayerManager playerManager;
 
