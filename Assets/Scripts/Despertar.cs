2556098d4907f315590db3f4bb8a1fad43ab6e30
n2t <vicentplambng@gmail.com>
2019-11-25 00:05:08 +0100

    parpadeo en pruebas y algun tweak a la nieve

---
+++ Assets/Scripts/Despertar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despertar : MonoBehaviour
+{
+    public GameObject topLid;
+    public GameObject bottomLid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Parpadear();
+    }
+
+    // Update is called once per frame
+    void Parpadear()
+    {
+        topLid.GetComponent<Animation>().Play("Parpadeo");
+        bottomLid.GetComponent<Animation>().Play("Parpadeo");
+    }
+}
