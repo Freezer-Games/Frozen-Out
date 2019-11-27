e038409ae960ee09ca3ee9ecd0b62929cbb567ad
Alejandro Gómez <algono@inf.upv.es>
2019-11-27 17:57:50 +0100

    Added text size to variable storage yarn

--- Assets/Scripts/Dialogue/VariableStorageYarn.cs
+++ Assets/Scripts/Dialogue/VariableStorageYarn.cs
@@ -7,30 +7,34 @@ namespace Assets.Scripts.Dialogue
 {
     public class VariableStorageYarn : VariableStorageBehaviour
     {
-        // Where we actually keep our variables
-        private Dictionary<string, Yarn.Value> variables = new Dictionary<string, Yarn.Value>();
-
         public Inventory inventory;
+
+        // Where we actually keep our variables
+        private readonly Dictionary<string, Yarn.Value> variables = new Dictionary<string, Yarn.Value>();
+
+        private GameManager gameManager;
 
         void Start()
         {
+            gameManager = FindObjectOfType<GameManager>();
             ResetToDefaults();
         }
 
         public override void ResetToDefaults ()
         {
-            Clear ();
+            Clear();
+
+            SetValue(nameof(gameManager.TextSize), gameManager.TextSize);
 
             // For each default variable that's been defined, parse the string
             // that the user typed in in Unity and store the variable
             foreach (ItemInfo item in inventory.inventoryItems) {
 
                 SetValue (item.variableName, item.isInitiallyInInventory);
-
             }
         }
 
-        public void SetValue(string variableName, bool value, bool includeLeading = true)
+        public void SetValue<T>(string variableName, T value, bool includeLeading = true)
         {
             Yarn.Value yarnValue = new Yarn.Value(value);
 
