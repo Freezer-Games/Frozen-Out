07937a01557f4970226897a44a4e6fab2de5bd37
Plorrios <pablolopezorrios@hotmail.es>
2019-11-28 11:28:18 +0100

    Dialogos guardias

--- Assets/Scripts/IA/perseguir.cs
+++ Assets/Scripts/IA/perseguir.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Yarn.Unity;
+using Assets.Scripts.Dialogue;
 
 public class perseguir : MonoBehaviour
 {
+    private DialogueRunner dialogueSystemYarn;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +16,6 @@ public class perseguir : MonoBehaviour
         List<Transform> visibles = gameObject.GetComponent<FieldOfView>().visibleTargets;
         List<Transform> cercanos = gameObject.GetComponent<FieldOfView>().closeTargets;
         if (visibles.Count>0 && cercanos.Count<2) { agent.destination = visibles[0].position; }
-        else if (cercanos.Count == 2) { agent.destination = gameObject.transform.position; }
+        else if (cercanos.Count == 2) { agent.destination = gameObject.transform.position; dialogueSystemYarn.StartDialogue(gameObject.GetComponent<NPCYarn>().talkToNode); }
     }
 }
