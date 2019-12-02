4029d14f8281ceca558ec1acbb01e7f9908b5851
Plorrios <pablolopezorrios@hotmail.es>
2019-12-02 14:30:00 +0100

    Fix Patrullas

--- Assets/Scripts/IA/patrullar.cs
+++ Assets/Scripts/IA/patrullar.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Yarn.Unity;
+using Assets.Scripts.Dialogue;
 
 public class patrullar : MonoBehaviour
 {
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
+    string estado = "patrullando";
+    private DialogueRunner dialogueSystemYarn;
+    bool hablar = false;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        dialogueSystemYarn = FindObjectOfType<DialogueRunner>();
         agent.autoBraking = false;
         GotoNextPoint();
     }
@@ -27,7 +34,43 @@ public class patrullar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!agent.pathPending && agent.remainingDistance < 0.1f)
-            GotoNextPoint();
+        List<Transform> visibles = gameObject.GetComponent<FieldOfView>().visibleTargets;
+        List<Transform> cercanos = gameObject.GetComponent<FieldOfView>().closeTargets;
+        switch (estado)
+        {
+            case "patrullando":
+                if (!agent.pathPending && agent.remainingDistance < 1f)
+                    GotoNextPoint();
+                if (visibles.Count > 0 && dialogueSystemYarn.isDialogueRunning && dialogueSystemYarn.currentNodeName != "Guardia")
+                { estado = "perseguir"; }
+                break;
+
+            case "perseguir":
+                if (hablar)
+                {
+                    dialogueSystemYarn.StartDialogue(gameObject.GetComponent<NPCYarn>().talkToNode);
+                    hablar = false;
+                    estado = "esperar";
+                }
+                else if (visibles.Count > 0 && cercanos.Count < 2 && dialogueSystemYarn.isDialogueRunning && dialogueSystemYarn.currentNodeName != "Guardia")
+                {
+                    agent.destination = visibles[0].position;
+                }
+                else if (cercanos.Count == 2 && dialogueSystemYarn.isDialogueRunning && dialogueSystemYarn.currentNodeName != "Guardia" && dialogueSystemYarn.currentNodeName != null)
+                {
+                    print(dialogueSystemYarn.currentNodeName);
+                    dialogueSystemYarn.Stop();
+                    hablar = true;
+                }
+                if (cercanos.Count == 2)
+                {
+                    agent.destination = gameObject.transform.position;
+                }
+                break;
+
+            case "esperar":
+                if (cercanos.Count == 0 && visibles.Count == 0) { estado = "patrullando"; }
+                break;
+        }
     }
 }
