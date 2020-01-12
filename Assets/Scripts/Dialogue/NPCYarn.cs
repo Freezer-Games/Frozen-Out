bdbc6e7b0b1f645a59d5c439b5aa61d2a05e7e09
vicen <vicentplambng@gmail.com>
2020-01-12 22:07:26 +0100

    Reorganizado script de animacion dialogo, indicador de dialogo en pruebas

--- Assets/Scripts/Dialogue/NPCYarn.cs
+++ Assets/Scripts/Dialogue/NPCYarn.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity;
 
 namespace Assets.Scripts.Dialogue
@@ -9,7 +11,51 @@ namespace Assets.Scripts.Dialogue
 
         public string characterName = "";
         public string talkToNode = "";
-        
+		
+		private Animator npcAnimator;
+		private Canvas indicator;
+		
+		void Start()
+		{
+			npcAnimator = GetComponent<Animator>();
+			indicator = GetComponentInChildren<Canvas>();
+			
+			if(characterName.Equals("")) {
+				characterName = talkToNode;
+			}
+			
+			DialogueRunner dialogueSystem = FindObjectOfType<DialogueRunner>();
+            dialogueSystem.Started += HideIndicatorWhenStarted;
+			dialogueSystem.Ended += ShowIndicatorWhenEnded;
+		}
+		
+		[YarnCommand("setanim")]
+		public void TriggerAnimation(string animationName) {
+			
+			if(npcAnimator != null) {				
+				animationName = "Anim_" + animationName;
+			
+				npcAnimator.SetTrigger(animationName);
+			}
+			
+		}
+		
+		private void HideIndicatorWhenStarted(object sender, DialogueEventArgs e)
+        {
+            if (indicator != null)
+            {
+                indicator.enabled = false;
+            }
+        }
+		
+		private void ShowIndicatorWhenEnded(object sender, EventArgs e)
+        {
+            if (indicator != null)
+            {
+                indicator.enabled = true;
+            }
+        }
+		
     }
 }
 
