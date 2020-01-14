e134043f65b9079ff2179900d983dfdf5e4ba9c1
vicen <vicentplambng@gmail.com>
2020-01-14 02:02:34 +0100

    tutti no se mueve ahora

--- Assets/Scripts/IA/TuttiEscapeController.cs
+++ Assets/Scripts/IA/TuttiEscapeController.cs
@@ -8,38 +8,45 @@ public class TuttiEscapeController : MonoBehaviour
 {
     public Transform firstDestino;
     public Transform secondDestino;
-
     
     private bool objetivo1 = false;
     private bool objetivo2 = false;
     private bool objetivo3 = false;
 
     private VariableStorageYarn variableStorageYarn;
-    private Animator jumpAnimator;
+    private Animator tuttiAnimator;
     private NavMeshAgent agent;
     
     void Start()
     {
         variableStorageYarn = FindObjectOfType<VariableStorageYarn>();
         agent = GetComponent<NavMeshAgent>();
-        jumpAnimator = GetComponent<Animator>();
+        tuttiAnimator = GetComponent<Animator>();
+		agent.updatePosition = false;
     }
     
     void Update()
     {
+		if(transform.position == firstDestino.position || transform.position == secondDestino.position) {
+			tuttiAnimator.SetBool("isMoving", false);
+		}
+		
         if (!objetivo1 && variableStorageYarn.GetBoolValue("Tutti_Escape1") == true) {
+			tuttiAnimator.SetBool("isMoving", true);
+			tuttiAnimator.Play("Caminado_Anticipacion");
             agent.destination = firstDestino.position;
             objetivo1 = true;
         }
         else if (!objetivo2 && variableStorageYarn.GetBoolValue("Tutti_Escape2") == true )
         {
+			tuttiAnimator.SetBool("isMoving", true);
             agent.destination = secondDestino.position;
             objetivo2 = true;
         }
         else if(!objetivo3 && variableStorageYarn.GetBoolValue("Tutti_Escape3") == true )
         {
-            agent.enabled = false;
-            jumpAnimator.enabled = true;
+			tuttiAnimator.SetBool("isMoving", true);
+            tuttiAnimator.enabled = true;
             objetivo3 = true;
         }
     }
