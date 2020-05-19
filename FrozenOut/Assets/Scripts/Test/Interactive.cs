1833e1099c93f0b8248e3e2f3daa4b266640fd4e
anleus <tomas.rui598@gmail.com>
2020-05-19 17:12:18 +0200

    picar esta

---
+++ FrozenOut/Assets/Scripts/Test/Interactive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactive : MonoBehaviour
+{
+    [SerializeField] Transform InteractionPoint;
+
+    public abstract void Execute();
+
+    public abstract void Advise();
+
+    public Transform GetInteractPoint()
+    {
+        return InteractionPoint;
+    }
+}
