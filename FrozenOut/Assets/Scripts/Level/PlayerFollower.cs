eb4047a4cbf299a1c1fbf8516ecbc01f92c882a2
n2t <vicentplambng@gmail.com>
2020-06-10 20:24:16 +0200

    Partículas de nevar

---
+++ FrozenOut/Assets/Scripts/Level/PlayerFollower.cs
@@ -0,0 +1,23 @@
+﻿using Scripts.Level.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Level
+{
+    public class PlayerFollower : MonoBehaviour
+    {
+        private PlayerManager PlayerManager => GameManager.Instance.CurrentLevelManager.GetPlayerManager();
+        private readonly float HeightOffset = 10.0f;
+
+        private void Update()
+        {
+            FollowPlayer(PlayerManager.Player.transform);
+        }
+
+        private void FollowPlayer(Transform playerTransform)
+        {
+            this.transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + HeightOffset, playerTransform.position.z);
+        }
+    }
+}
