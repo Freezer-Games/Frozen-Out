36b49d2b1f89b204a452002344c636f55d955ae3
Plorrios <pablolopezorrios@hotmail.es>
2020-07-04 17:53:21 +0200

    Animacion detectado completada

---
+++ FrozenOut/Assets/Scripts/IA/StartDetectionUIAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDetectionUIAnimation: MonoBehaviour
+{
+    Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+
+        anim.SetBool("Detectado", true);
+
+    }
+
+    public void StopAnimation()
+    {
+
+        anim.SetBool("Detectado", false);
+
+    }
+
+}
