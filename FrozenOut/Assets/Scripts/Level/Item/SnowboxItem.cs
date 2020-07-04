2b396ab2c3c84d29b388c437c48531d371e913e8
n2t <vicentplambng@gmail.com>
2020-07-04 20:49:59 +0200

    Trigger de bola saliendo de caja

---
+++ FrozenOut/Assets/Scripts/Level/Item/SnowboxItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Scripts.Level.Dialogue;
+
+namespace Scripts.Level.Item
+{
+    public class SnowboxItem : ItemUser
+    {
+        public Animator BoxAnimator;
+        public DialogueActer UnableTalker;
+
+        private DialogueManager DialogueManager => GameManager.Instance.CurrentLevelManager.GetDialogueManager();
+
+        public override void OnPlayerCol() {}
+
+        public override void OnPlayerExitCol() {}
+
+        public override void OnUse()
+        {
+            BoxAnimator.SetTrigger("BallJump");
+        }
+
+        public override void OnUnableUse()
+        {
+            DialogueManager.StartDialogue(UnableTalker);
+        }
+    }
+}
