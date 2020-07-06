269d19cb5482c70bec6b90e1deeb0db93104e5df
n2t <vicentplambng@gmail.com>
2020-07-06 18:32:51 +0200

    Menu con transicion Creditos empezados

--- FrozenOut/Assets/Scripts/Menu/Main/MainMenuCameraTransition.cs
+++ FrozenOut/Assets/Scripts/Menu/Main/MainMenuCameraTransition.cs
@@ -6,24 +6,16 @@ namespace Scripts.Menu.Main
 {
     public class MainMenuCameraTransition : MonoBehaviour
     {
-        //public CinemachineVirtualCamera MainCamera;
-        //public CinemachineVirtualCamera MiddleCamera;
-        //public CinemachineVirtualCamera OptionsCamera;
         public CinemachineVirtualCamera CamaraRail;
 
         public GameObject ScreenCamera;
         public GameObject WhiteOverlay;
-        public CinemachineTrackedDolly Dolly;
 
-        private int MainCameraPriority;
-        private int MiddleCameraPriority;
-        private const float TransitionDelay = 1.0f;
+        private CinemachineTrackedDolly Dolly;
 
         void Start()
         {
             Dolly = CamaraRail.GetCinemachineComponent<CinemachineTrackedDolly>();
-            //MainCameraPriority = MainCamera.Priority;
-            //MiddleCameraPriority = MiddleCamera.Priority;
             StartCoroutine(ActiveMenu(true));
         }
 
@@ -32,14 +24,11 @@ namespace Scripts.Menu.Main
             ScreenCamera.SetActive(false);
             WhiteOverlay.SetActive(false);
             Dolly.m_PathPosition = 2;
-            //StartCoroutine(DoGoToOptions());
         }
 
         public void GoToMain()
         {
             StartCoroutine(ActiveMenu(false));
-
-            //StartCoroutine(DoGoToMain());
         }
 
         private IEnumerator ActiveMenu(bool start)
@@ -52,29 +41,6 @@ namespace Scripts.Menu.Main
             WhiteOverlay.SetActive(true);
             yield return new WaitForSeconds(1.0f);
             ScreenCamera.SetActive(true);
-           
-        }
-
-        private IEnumerator DoGoToMain()
-        {
-            //MiddleCamera.Priority = MiddleCameraPriority;
-
-            yield return new WaitForSeconds(TransitionDelay);
-
-            //MainCamera.Priority = MainCameraPriority;
-
-            yield return new WaitForSeconds(TransitionDelay);
-        }
-
-        private IEnumerator DoGoToOptions()
-        {
-            //MainCamera.Priority = 0;
-
-            yield return new WaitForSeconds(TransitionDelay);
-
-            //MiddleCamera.Priority = 0;
-
-            yield return new WaitForSeconds(TransitionDelay);
         }
     }
 }
