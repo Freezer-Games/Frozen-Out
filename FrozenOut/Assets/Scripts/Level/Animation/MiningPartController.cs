efab29585754b37ea12606c3425ed3847b1d4182
anleus <tomas.rui598@gmail.com>
2020-07-17 18:04:34 +0200

    subete pls

---
+++ FrozenOut/Assets/Scripts/Level/Animation/MiningPartController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Level.Animation
+{
+    public class MiningPartController : MonoBehaviour
+    {
+        public GameObject MiningParticles;
+
+        public void ManageParticles()
+        {
+            StartCoroutine(PlayParticles());
+        }
+
+        private IEnumerator PlayParticles()
+        {
+            MiningParticles.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            MiningParticles.SetActive(false);
+        }
+    }
+}
